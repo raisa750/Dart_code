ed3dfcc524232cb12d9172d27401de970a39c3e1
raisa750 <raisaiqbal302@gmail.com>
2026-03-03 21:13:26 +0600

    Delete practice question  3 directory

--- practice question  3/11.dart
+++
@@ -1,9 +0,0 @@
-void createUser(String name, int age, {bool isActive = true}) {
-  print(name);
-  print(age);
-  print(isActive);
-}
-
-void main() {
-  createUser("Raisa", 22, isActive: true);
-}
--- practice question  3/8.dart
+++
@@ -1,8 +0,0 @@
-int add(int a, int b) {
-  return a + b;
-}
-
-void main() {
-  int sum = add(9, 7); 
-  print(sum);
-}
