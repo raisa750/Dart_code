7ea9410b22aa4a5a849e4f1e37880ce5ca04d1ad
raisa750 <raisaiqbal302@gmail.com>
2026-03-03 21:00:11 +0600

    Delete practice question  3 directory

--- practice question  3/1.dart
+++
@@ -1,8 +0,0 @@
-void printName() {
-  String name = "Raisa";
-  print("My name is $name");
-}
-
-void main() {
-  printName();
-}
--- practice question  3/3.dart
+++
@@ -1,8 +0,0 @@
-void greet(String name) {
-  print("Hello, $name");
-}
-
-void main() {
-  greet("John");
-  greet("Alice");
-}
